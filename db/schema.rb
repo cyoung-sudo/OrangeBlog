11ac3f95060d4a66e6de16da7947f8a44b3b671d
“cyoung-sudo” <cyoungcs20@gmail.com>
2020-03-14 14:02:59 -0700

    added article categories association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_203148) do
+ActiveRecord::Schema.define(version: 2020_03_14_204439) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
